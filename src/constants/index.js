990f795a3a3753176013ddd23011f338615dbca7
Azeem46 <shaikhazeem4646@gmail.com>
2024-01-17 22:41:52 +0530

    added link in footer for github, and linkedin

--- src/constants/index.js
+++ src/constants/index.js
@@ -169,12 +169,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/Azeem46',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/azeem-shaikh-02b686263/',
     }
 ];
 
